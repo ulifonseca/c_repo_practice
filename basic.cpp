6e8abbec8cf59e9dff340162749424b149dda911
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-07 08:45:52 -0600

    String funtions

--- basic.cpp
+++ basic.cpp
@@ -6,10 +6,15 @@ using std::endl;
 
 int main()
 {
-    std::string name;
-    getline(std::cin,name);
+    std::string name = "Miriam Arreola";
+    name.append(" Hernandez");
+    name.insert(7,"Love ");
+    name.erase(7,5);
+    name.pop_back();
+    name.pop_back();
+    name.push_back('e');
+    name.push_back('z');
     cout<<name<<endl;
-    
     return 0;    
 }
 
