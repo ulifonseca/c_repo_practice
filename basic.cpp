b918b8ad4bfea15571cb65f2b234596db4271a49
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-07 08:24:27 -0600

    String with functions

--- basic.cpp
+++ basic.cpp
@@ -7,11 +7,17 @@ using std::endl;
 int main()
 {
     std::string name = "Miriam";
+    std::string name2;  //Initially "" declared empty
+    std::string lastname = " Arreola";
+    std::string completeName = name + lastname;  
     cout<<"Name and ASCII value..."<<endl;
     for (int i = 0; i <= name.length(); i++) //It includes the null character '\0' 
     {
         cout<<name[i]<<" --> "<<(int)name[i]<<endl;      
     }
+    cout<<name2<<endl;
+    //String concatenation
+    cout<<completeName.length()<<endl;
     
     return 0;    
 }
