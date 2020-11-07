df52abd600c02ea5b77763b173ae752554dd1154
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-07 09:42:42 -0600

    Switch with enum

--- basic.cpp
+++ basic.cpp
@@ -6,17 +6,31 @@ using std::endl;
 
 int main()
 {
-    int day = 3;
-    switch (day)
+    enum seasons
     {
-    case 1:
-        cout << "Monday" << endl;
+        spring = 1,
+        summer = 10,
+        fall = 100,
+        winter = 1000
+    };
+    seasons now = spring;
+
+    switch (now)
+    {
+    case spring:
+        cout << "It´s sunny" << endl;
         break;
-    case 2:
-        cout << "Tuesday" << endl;
+    case summer:
+        cout << "It´s very sunny, you need to go swimming!" << endl;
+        break;
+    case fall:
+        cout << "It´s cold" << endl;
+        break;
+    case winter:
+        cout << "It´s very cold" << endl;
         break;
     default:
-        cout << "Not a day" << endl;
+        cout << "Not a season" << endl;
         break;
     }
     return 0;
