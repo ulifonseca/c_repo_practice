f26e495123bb3f29c1868a4b6a3af7ff07e475ee
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-07 08:06:46 -0600

    String version 1

--- basic.cpp
+++ basic.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using std::cout; // using directives 
 using std::endl; 
 
 int main()
 {
-    cout<<pow(2,3)<<endl;
-    cout<<sqrt(25)<<endl;
-    cout<<10%3<<endl;
-    cout<<remainder(10,3.25)<<endl;
-    cout<<fmax(23.2,23.3)<<endl;
-    cout<<floor(3.5)<<endl;
-    cout<<ceil(2.3)<<endl;
+    std::string name = "Miriam";
+    cout<<"Name and ASCII value..."<<endl;
+    for (int i = 0; i <= name.length(); i++) //It includes the null character '\0' 
+    {
+        cout<<name[i]<<" --> "<<(int)name[i]<<endl;      
+    }
+    
     return 0;    
 }
 
