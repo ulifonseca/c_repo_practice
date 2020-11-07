72487034e717d5407a09d6903a4ef4fc90a1412c
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-06 21:20:48 -0600

    Basics 1
    Adding printf

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+ using std::cout; // using directives 
+ using std::endl; 
+int main()
+{
+    int slices = 5; //Local variable declaration and initialization
+    //cout<<"Hello miry"<<endl;
+    cout<<"Number of slices: "<<slices<<endl;
+    printf("You have %i slices of pizza\n",slices);
+    return 0;
+}
