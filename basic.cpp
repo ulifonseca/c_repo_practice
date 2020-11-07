0989607782273fea709d4ea95430fb9319e93292
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-06 22:00:24 -0600

    pow function with cmath

--- basic.cpp
+++ basic.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <stdio.h>
+#include <cmath>
 
  using std::cout; // using directives 
  using std::endl; 
  
-int square(int base);
 int main()
 {
     int base = 0; //Local variable declaration and initialization
     int exponent = 0;
     cout<<"Base: ";
     std::cin>>base;
-    cout<<square(base)<<endl;
+    cout<<"Exponent: ";
+    std::cin>>exponent;
+    cout<<pow(base,exponent)<<endl;
     
     return 0;
 }
-int square(int base)
-{
-    return base*base;
-}
+
 
