ad5b11824e8288d4f41485f8d80a4f9b8cb80b90
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-06 21:54:24 -0600

    square function

--- basic.cpp
+++ basic.cpp
@@ -4,15 +4,19 @@
  using std::cout; // using directives 
  using std::endl; 
  
+int square(int base);
 int main()
 {
-    int slices = 0; //Local variable declaration and initialization
-    int sodas = 0;
-    cout<<"Number of slices of pizza: ";
-    std::cin>>slices;
-    cout<<"Number of sodas: ";
-    std::cin>>sodas;
-    cout<<slices<<" slices of pizza and "<<sodas<<" sodas."<<endl;
+    int base = 0; //Local variable declaration and initialization
+    int exponent = 0;
+    cout<<"Base: ";
+    std::cin>>base;
+    cout<<square(base)<<endl;
     
     return 0;
 }
+int square(int base)
+{
+    return base*base;
+}
+
