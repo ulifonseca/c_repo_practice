913e892371ba61e67b0a1828e1661222ccfd3e1a
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-07 08:57:06 -0600

    String funtions2

--- basic.cpp
+++ basic.cpp
@@ -7,6 +7,7 @@ using std::endl;
 int main()
 {
     std::string name = "Miriam Arreola";
+    std::string name2 = "Ulises";
     name.append(" Hernandez");
     name.insert(7,"Love ");
     name.erase(7,5);
@@ -14,6 +15,15 @@ int main()
     name.pop_back();
     name.push_back('e');
     name.push_back('z');
+    if(name2.compare("Ulises")==0)
+    {
+        cout<<"Equal!"<<endl;
+    }
+    else
+    {
+        cout<<"Not equal"<<endl;
+    }
+    
     cout<<name<<endl;
     return 0;    
 }
