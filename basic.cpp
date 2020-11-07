5f703a86dc29d3d1ecd1743871c29097a21224bd
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-07 09:32:54 -0600

    If else if statement

--- basic.cpp
+++ basic.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <string>
 
-using std::cout; // using directives 
-using std::endl; 
+using std::cout; // using directives
+using std::endl;
 
 int main()
 {
-    int x = 0xFF;
-    int y = 255;
-    cout<<x<<endl;
-    cout<<std::hex<<y<<endl;
-    return 0;    
-}
+    int age;
+    bool value = true;
+    cout << "Enter age: ";
 
+    std::cin >> age;
+    if (age > 18 && value)
+    {
+        cout << "You are above eighteen!" << endl;
+    }
+    else if (age < 18)
+    {
+        cout << "You are under eighteen!" << endl;
+    }
+    else
+    {
+        cout << "You are eighteen years old!" << endl;
+    }
+
+    return 0;
+}
