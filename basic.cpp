ec41a2021ebc6345556234182fa4165983f54ea5
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-06 22:57:39 -0600

    cmath functions

--- basic.cpp
+++ basic.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#define PI 3.14159
+#include <cmath>
 
 using std::cout; // using directives 
 using std::endl; 
 
 int main()
 {
-    const int age = 23;
-    cout<<"Age : "<<age<<endl;
-    cout<<"PI  : "<<PI<<endl;
+    cout<<pow(2,3)<<endl;
+    cout<<sqrt(25)<<endl;
+    cout<<10%3<<endl;
+    cout<<remainder(10,3.25)<<endl;
+    cout<<fmax(23.2,23.3)<<endl;
+    cout<<floor(3.5)<<endl;
+    cout<<ceil(2.3)<<endl;
     return 0;    
 }
 
