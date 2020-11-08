3ec8580a8498f5bef0b1c2454cbc17ab26136143
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-08 09:45:42 -0600

    Arrays random and input user

--- basic.cpp
+++ basic.cpp
@@ -19,13 +19,46 @@ void display_array(int a[], int size)
         cout << a[i] << " ";
     }
     cout << endl;
+    cout << endl;
 }
 int main()
 {
     int a[5] = {};
+    int option = 0;
     int size = sizeof(a) / sizeof(int);
     srand(time(NULL));
-    fill_array(a, size, 5, 10);
-    display_array(a, size);
+
+    do
+    {
+        cout << "\n(1) User input fill array\n(2) Random filling\n(3) Exit" << endl
+             << endl
+             << "Option: ";
+        std::cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            cout << "Enter " << size << " numbers:" << endl;
+            for (int i = 0; i < size; i++)
+            {
+                cout << "Iindex [" << i << "] --> ";
+                std::cin >> a[i];
+            }
+            cout << endl;
+            display_array(a, size);
+            break;
+
+        case 2:
+            cout << "Random filling..." << endl;
+            fill_array(a, size, 90, 100);
+            display_array(a, size);
+            break;
+        default:
+            cout << "Bye" << endl;
+            break;
+        }
+
+    } while (option != 3);
+
     return 0;
 }
