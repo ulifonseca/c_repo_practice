6a0dc2017b285c6503cd3a67a21d3ed779a3a3d9
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-08 12:06:02 -0600

    Vector program without erase

--- basic.cpp
+++ basic.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <vector>
 
 using std::cout;
 using std::endl;
 
-void fill_array(int a[], int size, int min, int max)
+void fill_vector(std::vector<int> &a, int size, int min, int max)
 {
     for (int i = 0; i < size; i++)
     {
-        a[i] = (rand() % (max - min + 1) + min);
+        a.push_back(rand() % (max - min + 1) + min);
     }
 }
-void display_array(int a[], int size)
+void display_vector(std::vector<int> a)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < a.size(); i++)
     {
         cout << a[i] << " ";
     }
     cout << endl;
     cout << endl;
 }
+void erase_vector(std::vector<int> &a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        a.pop_back();
+    }
+}
+
 int main()
 {
-    int a[5] = {};
+    int size = 2;
+    std::vector<int> vector1;
     int option = 0;
-    int size = sizeof(a) / sizeof(int);
     srand(time(NULL));
-
     do
     {
-        cout << "\n(1) User input fill array\n(2) Random filling\n(3) Exit" << endl
+        int v = 0;
+        cout << "Vector..." << endl;
+        cout << "\n(1) User input fill vector\n(2) Random filling\n(3) Display vector\n(4) Erase vector\n(5) Exit" << endl
              << endl
              << "Option: ";
         std::cin >> option;
@@ -42,26 +52,36 @@ int main()
             for (int i = 0; i < size; i++)
             {
                 cout << "Index [" << i << "] --> ";
-                if (!(std::cin >> a[i]))
-                {
-                    option = 3;
-                }
+                std::cin >> v;
+                vector1.push_back(v);
             }
             cout << endl;
-            display_array(a, size);
+            display_vector(vector1);
             break;
 
         case 2:
             cout << "Random filling..." << endl;
-            fill_array(a, size, 90, 100);
-            display_array(a, size);
+            fill_vector(vector1, size, 90, 100);
+            display_vector(vector1);
             break;
+        case 3:
+            cout << "Displaying vector..." << endl;
+            display_vector(vector1);
+            cout << endl;
+            cout << endl;
+            break;
+        case 4:
+            cout << "Erasing vector" << endl;
+            erase_vector(vector1);
+            cout << "Finish." << endl;
+            break;
+
         default:
             cout << "Bye" << endl;
             break;
         }
 
-    } while (option != 3);
+    } while (option != 5);
 
     return 0;
 }
