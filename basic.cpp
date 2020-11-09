19ed4725d5d830fcf8a53e2874f7b69ed293a519
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-08 22:13:09 -0600

    Vector push pop dispaly

--- basic.cpp
+++ basic.cpp
@@ -1,87 +1,62 @@
 #include <iostream>
-#include <string>
-#include <time.h>
 #include <vector>
 
 using std::cout;
 using std::endl;
 
-void fill_vector(std::vector<int> &a, int size, int min, int max)
+void display_vector(std::vector<int> &vec1)
 {
-    for (int i = 0; i < size; i++)
+    cout << "Displaying array..." << endl;
+    for (int i = 0; i < vec1.size(); i++)
     {
-        a.push_back(rand() % (max - min + 1) + min);
-    }
-}
-void display_vector(std::vector<int> a)
-{
-    for (int i = 0; i < a.size(); i++)
-    {
-        cout << a[i] << " ";
+        cout << vec1[i] << " ";
     }
     cout << endl;
-    cout << endl;
 }
-void erase_vector(std::vector<int> &a)
+
+void push_vector(std::vector<int> &vec1, int value)
 {
-    for (int i = 0; i < a.size(); i++)
-    {
-        a.pop_back();
-    }
+    vec1.push_back(value);
+}
+
+void pop_vector(std::vector<int> &vec1)
+{
+    vec1.pop_back();
 }
 
 int main()
 {
-    int size = 2;
-    std::vector<int> vector1;
     int option = 0;
-    srand(time(NULL));
+    int value = 0;
+    std::vector<int> vector1;
+
     do
     {
-        int v = 0;
-        cout << "Vector..." << endl;
-        cout << "\n(1) User input fill vector\n(2) Random filling\n(3) Display vector\n(4) Erase vector\n(5) Exit" << endl
-             << endl
-             << "Option: ";
+        cout << endl;
+        cout << "(1)Push vector.\n(2)Pop vector\n(3)Display vector.\n(4)Exit.\nOption? ";
         std::cin >> option;
-
         switch (option)
         {
         case 1:
-            cout << "Enter " << size << " numbers:" << endl;
-            for (int i = 0; i < size; i++)
-            {
-                cout << "Index [" << i << "] --> ";
-                std::cin >> v;
-                vector1.push_back(v);
-            }
+            cout << "Push vector..." << endl;
+            cout << "Value: ";
+            std::cin >> value;
+            push_vector(vector1, value);
             cout << endl;
-            display_vector(vector1);
             break;
-
         case 2:
-            cout << "Random filling..." << endl;
-            fill_vector(vector1, size, 90, 100);
-            display_vector(vector1);
-            break;
+            cout << "Pop vector..." << endl;
+            pop_vector(vector1);
+            cout << endl;
         case 3:
-            cout << "Displaying vector..." << endl;
             display_vector(vector1);
             cout << endl;
-            cout << endl;
-            break;
-        case 4:
-            cout << "Erasing vector" << endl;
-            erase_vector(vector1);
-            cout << "Finish." << endl;
-            break;
-
         default:
-            cout << "Bye" << endl;
             break;
         }
 
-    } while (option != 5);
+        /* code */
+    } while (option != 4);
 
     return 0;
 }
